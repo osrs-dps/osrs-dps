352ec897da54cd7713b3cc695b046b8532cea9be
Alex Jensen <ajensen@rednovalabs.com>
2020-04-02 14:01:23 -0600

    real initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="equipment-wrapper">
+            <div className="margin-tb">
+                <select id="helmet" className="equipment-slot">
+                    <option value="">Helmet</option>
+                    <option value="">hat</option>
+                </select>
+            </div>
+
+            <div className="margin-tb">
+                <select id="ammo" className="equipment-slot">
+                    <option value="">Ammo/Blessing</option>
+                    <option value="">hat</option>
+                </select>
+            </div>
+        </div>
+
+        <div className="stats-wrapper">
+            <div calss="margin-tb">
+                <label className="stat-label" for="rsn">RSN</label>
+                <input className="stat-input" type="text" id="rsn"></input>
+            </div>
+            <div calss="margin-tb">
+                <label className="stat-label" for="att">Attack</label>
+                <input className="stat-input" type="number" id="attack"></input>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Potion</option>
+                    <option value="">hat</option>
+                </select>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Prayer</option>
+                    <option value="">hat</option>
+                </select>
+            </div>
+            <div calss="margin-tb">
+                <label className="stat-label" for="str">Strength</label>
+                <input className="stat-input" type="number" id="str"></input>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Potion</option>
+                    <option value="">hat</option>
+                </select>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Prayer</option>
+                    <option value="">hat</option>
+                </select>
+            </div>
+            <div calss="margin-tb">
+                <label className="stat-label" for="def">Defence</label>
+                <input className="stat-input" type="number" id="def"></input>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Potion</option>
+                    <option value="">hat</option>
+                </select>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Prayer</option>
+                    <option value="">hat</option>
+                </select>
+            </div>
+            <div calss="margin-tb">
+                <label className="stat-label" for="magic">Magic</label>
+                <input className="stat-input" type="number" id="magic"></input>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Potion</option>
+                    <option value="">hat</option>
+                </select>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Prayer</option>
+                    <option value="">hat</option>
+                </select>
+            </div>
+            <div calss="margin-tb">
+                <label className="stat-label" for="range">Ranged</label>
+                <input className="stat-input" type="number" id="range"></input>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Potion</option>
+                    <option value="">hat</option>
+                </select>
+                <select id="att-pot" className="stat-input">
+                    <option value="">Prayer</option>
+                    <option value="">hat</option>
+                </select>
+            </div>
+            <div calss="margin-tb">
+                <label className="stat-label" for="hp">Hitpoints</label>
+                <input className="stat-input" type="number" id="hp"></input>
+            </div>
+            <div calss="margin-tb">
+                <label className="stat-label" for="prayer">Prayer</label>
+                <input className="stat-input" type="number" id="prayer"></input>
+            </div>
+        </div>
+
+        <div className="monster-wrapper">
+            <select id="att-pot" className="">
+                <option value="">Monster</option>
+                <option value="">hat</option>
+            </select>
+        </div>
     </div>
   );
 }
