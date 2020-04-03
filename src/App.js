9565da7dd617de4bb72ef8bd30178afbf0535ff9
Xingyuan Li <xingyuan.li@uconn.edu>
2020-04-03 11:45:02 -0400

    added all equip slots with react-select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,177 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Select from 'react-select';
+
+const weaponSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const ammoSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+
+const headSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const capeSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const amuletSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const chestSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const legsSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const shieldSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const glovesSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const bootsSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
+
+const ringSlotOptions = [
+    {value: 'ancestralHat', label: 'Ancestral Hat'},
+    {value: 'robinHoodHat', label: 'Robin Hood Hat'},
+    {value: 'serpentineHelm', label: 'Serpentine Helm'},
+];
 
 function App() {
   return (
     <div className="App">
         <div className="equipment-wrapper">
             <div className="margin-tb">
-                <select id="helmet" className="equipment-slot">
-                    <option value="">Helmet</option>
-                    <option value="">hat</option>
-                </select>
+                <Select 
+                    id="weapon" 
+                    className="equipment-slot"
+                    placeholder="Select Weapon..."
+                    options={weaponSlotOptions}
+                />
             </div>
 
             <div className="margin-tb">
-                <select id="ammo" className="equipment-slot">
-                    <option value="">Ammo/Blessing</option>
-                    <option value="">hat</option>
-                </select>
+                <Select 
+                    id="ammo" 
+                    className="equipment-slot"
+                    placeholder="Select Ammo..."
+                    options={ammoSlotOptions}
+                />
+            </div>
+
+            <div className="margin-tb">
+                <Select 
+                    id="head" 
+                    className="equipment-slot"
+                    placeholder="Select Head..."
+                    options={headSlotOptions}
+                />
+            </div>
+
+            <div className="margin-tb">
+                <Select 
+                    id="cape" 
+                    className="equipment-slot"
+                    placeholder="Select Cape..."
+                    options={capeSlotOptions}
+                />
+            </div>
+
+            <div className="margin-tb">
+                <Select 
+                    id="amulet" 
+                    className="equipment-slot"
+                    placeholder="Select Amulet..."
+                    options={amuletSlotOptions}
+                />
+            </div>
+
+            <div className="margin-tb">
+                <Select 
+                    id="chest" 
+                    className="equipment-slot"
+                    placeholder="Select Chest..."
+                    options={chestSlotOptions}
+                />
+            </div>
+
+            <div className="margin-tb">
+                <Select 
+                    id="legs" 
+                    className="equipment-slot"
+                    placeholder="Select Legs..."
+                    options={legsSlotOptions}
+                />
+            </div>
+
+            <div className="margin-tb">
+                <Select 
+                    id="shield" 
+                    className="equipment-slot"
+                    placeholder="Select Shield..."
+                    options={shieldSlotOptions}
+                />
+            </div>
+
+            <div className="margin-tb">
+                <Select 
+                    id="gloves" 
+                    className="equipment-slot"
+                    placeholder="Select Gloves..."
+                    options={glovesSlotOptions}
+                />
+            </div>
+
+            <div className="margin-tb">
+                <Select 
+                    id="boots" 
+                    className="equipment-slot"
+                    placeholder="Select Boots..."
+                    options={bootsSlotOptions}
+                />
+            </div>
+
+            
+            <div className="margin-tb">
+                <Select 
+                    id="ring" 
+                    className="equipment-slot"
+                    placeholder="Select Ring..."
+                    options={ringSlotOptions}
+                />
             </div>
         </div>
 
