9fd89bd08125ced63312f9e73f7ee654bea5e8d0
Alex <96dalex@gmail.com>
2020-04-05 23:41:30 -0600

    calculate strength bonus

---
+++ src/slot_data.js
@@ -0,0 +1,25 @@
+import weaponSlot from './data/1h_weapon_slot.json';
+import ammoSlot from './data/ammo_blessing.json';
+import headSlot from './data/head_slot.json';
+import capeSlot from './data/cape_slot.json';
+import amuletSlot from './data/neck_slot.json';
+import chestSlot from './data/chest_slot.json';
+import legsSlot from './data/leg_slot.json';
+import shieldSlot from './data/shield_slot.json';
+import glovesSlot from './data/hand_slot.json';
+import bootsSlot from './data/boot_slot.json';
+import ringSlot from './data/ring_slot.json';
+
+export default {
+    weapon: weaponSlot,
+    ammo: ammoSlot,
+    head: headSlot,
+    cape: capeSlot,
+    amulet: amuletSlot,
+    chest: chestSlot,
+    legs: legsSlot,
+    shield: shieldSlot,
+    gloves: glovesSlot,
+    boots: bootsSlot,
+    ring: ringSlot,
+};
